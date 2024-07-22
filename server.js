04fbabcdfabfcf20ea1e22815d8b120e3953dcb1
Zainab Karim <zainab@DESKTOP-PIGI0AO.>
2024-07-22 13:07:06 +0300

    setup templating ejs

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 // MIDDLEWARE
 app.use(morgan('dev'));
 
+//check the run
+// app.get("/", async (req, res) => {
+//     res.send("hello, friend!");
+// });
+
+//Home page
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
